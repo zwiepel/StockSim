f34995c73175cf7f90161db5c0055104444a279b
M_Kt <maximilian.keet@gmail.com>
2025-05-05 09:56:41 +0200

    Scrollview Update +All Category

--- Assets/Scripting/MarketUI.cs
+++ Assets/Scripting/MarketUI.cs
@@ -10,6 +10,7 @@ public class MarketUI : MonoBehaviour
     public Transform companyListContent;
     public GameObject companyItemPrefab;
 
+    private bool showingAll = true;
     private CompanyCategory currentCategory = CompanyCategory.Tech;
 
     void Start()
@@ -21,18 +22,22 @@ public class MarketUI : MonoBehaviour
     public void ShowMarket()
     {
         panelMarket.SetActive(true);
-        FilterCompanies((int)currentCategory);
+        FilterCompanies(categoryDropdown.value);
     }
 
     public void HideMarket()
     {
         panelMarket.SetActive(false);
+        UIManager.Instance.panelMainUI.SetActive(true);
     }
 
     void SetupCategoryDropdown()
     {
         categoryDropdown.ClearOptions();
         var options = new List<string>();
+
+        options.Add("All"); // Wichtig: "All" hinzufügen
+
         foreach (var category in System.Enum.GetValues(typeof(CompanyCategory)))
         {
             options.Add(category.ToString());
@@ -40,24 +45,46 @@ public class MarketUI : MonoBehaviour
 
         categoryDropdown.AddOptions(options);
         categoryDropdown.onValueChanged.AddListener(FilterCompanies);
+
+        categoryDropdown.value = 0; // Setze Dropdown auf "All" standardmäßig
     }
 
     void FilterCompanies(int selectedIndex)
     {
-        currentCategory = (CompanyCategory)selectedIndex;
-
         foreach (Transform child in companyListContent)
         {
             Destroy(child.gameObject);
         }
 
-        if (MarketManager.Instance.categorizedCompanies.TryGetValue(currentCategory, out var companyList))
+        if (selectedIndex == 0)
         {
-            foreach (var company in companyList)
+            // Zeige ALLE Firmen
+            foreach (var categoryList in MarketManager.Instance.categorizedCompanies.Values)
             {
-                var item = Instantiate(companyItemPrefab, companyListContent);
-                item.GetComponent<CompanyItemUI>().SetCompany(company);
+                foreach (var company in categoryList)
+                {
+                    CreateCompanyItem(company);
+                }
             }
         }
+        else
+        {
+            // Zeige Firmen einer bestimmten Kategorie
+            currentCategory = (CompanyCategory)(selectedIndex - 1); // Achtung: -1 weil "All" am Anfang eingefügt wurde
+
+            if (MarketManager.Instance.categorizedCompanies.TryGetValue(currentCategory, out var companyList))
+            {
+                foreach (var company in companyList)
+                {
+                    CreateCompanyItem(company);
+                }
+            }
+        }
+    }
+
+    void CreateCompanyItem(Company company)
+    {
+        var item = Instantiate(companyItemPrefab, companyListContent);
+        item.GetComponent<CompanyItemUI>().SetCompany(company);
     }
 }
